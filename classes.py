025d1fa4c307a9e4a9eb6fc2760ba9fb10575d47
Алексей Чурсин <76111160+alekela@users.noreply.github.com>
2023-09-26 22:49:35 +0300

    update files

--- classes.py
+++ classes.py
@@ -27,8 +27,15 @@ class Ball:
         if 3 + self.radius >= self.y or self.y >= h - 3 - self.radius:
             self.Vy = -self.Vy
 
-    def collide_with_platform(self):
-        self.Vy = -self.Vy
+    def collide_with_platform(self, other):
+        other_x, other_y = other.get_pos()
+        other_dx, other_dy = other.get_sizes()
+        if other_x - other_dx / 2 < self.x < other_x + other_dx / 2:
+            if self.y <= other_y + other_dy / 2 and self.y >= other_y - other_dy / 2:
+                self.Vy = -self.Vy
+
+    def endgame(self, field):
+        return not (self.y >= field.get_size()[1] - 5)
 
 
 class Platform:
@@ -37,15 +44,19 @@ class Platform:
         self.x = w / 2
         self.y = 5 / 6 * h
         self.dy = 10
-        self.dx = 50
+        self.dx = 100
         pygame.draw.rect(field, 'white', [self.x - self.dx / 2, self.y - self.dy / 2, self.dx, self.dy], 0)
 
     def get_pos(self):
         return self.x, self.y
 
+    def get_sizes(self):
+        return self.dx, self.dy
+
     def update(self, field, dx):
         pygame.draw.rect(field, 'black', [self.x - self.dx / 2, self.y - self.dy / 2, self.dx, self.dy], 0)
-        self.x += dx
+        if self.x + dx + self.dx / 2 < field.get_size()[0] - 2 and self.x + dx - self.dx / 2 > 2:
+            self.x += dx
         self.redraw(field)
 
     def redraw(self, field):
@@ -53,6 +64,13 @@ class Platform:
 
 
 class Brick:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.dx = 10
+        self.dy = 5
+
+    def get_sizes(self):
+        return self.dx, self.dy
 
 
--- main.py
+++ main.py
@@ -9,12 +9,33 @@ pygame.draw.rect(screen, 'white', [1, 1, w - 2, h - 2], 2)
 
 running = True
 ball = Ball(screen, 300, 494)
+platform = Platform(screen)
+
 fps = 10
 clock = pygame.time.Clock()
+platform_right = False
+platform_left = False
 while running:
     clock.tick(1000 // fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                platform_left = True
+            if event.key == pygame.K_d:
+                platform_right = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                platform_left = False
+            if event.key == pygame.K_d:
+                platform_right = False
+    if platform_right:
+        platform.update(screen, 1)
+    if platform_left:
+        platform.update(screen, -1)
     ball.update(screen, 1 / fps)
+    ball.collide_with_platform(platform)
+    running = ball.endgame(screen)
+    pygame.draw.rect(screen, 'white', [1, 1, w - 2, h - 2], 2)
     pygame.display.flip()
